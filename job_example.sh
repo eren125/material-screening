189751e897dcf70c5192f086b4a06bd5bf229cc8
Emmanuel Ren <emmanuelren1995@gmail.com>
2021-03-22 16:57:31 +0100

    change environment management

--- job_example.sh
+++ job_example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-export RASPASCREEN=/home/emmanuel/Documents/These_1/08_Raspa_screening 
+export RASPASCREEN=/home/emmanuel/Documents/raspa-screener
 #export NODES=$(srun hostname | sort | uniq)
 #echo "Running on nodes:"
 #echo "${NODES}"
 
-python3 $RASPASCREEN/raspascreen.py -p 20 -n 20 -s $RASPASCREEN/data/structures_done.csv -m xenon -ps 0.1 1 10 1e2 1e3 1e4 1e5 -N 100000 -t ads -o .
+python3 $RASPASCREEN/raspascreen.py -p 20 -n 20 -s $RASPASCREEN/data/structures_sample.csv -m xenon krypton -N 100000 -t grid 
