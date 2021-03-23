2d5823f03a32aeadc3ed80903cb78aa4f9b610cf
Emmanuel Ren <emmanuelren1995@gmail.com>
2021-03-23 14:25:05 +0100

    remove visualisation files and restart (unused)

--- job_ads.sh
+++ job_ads.sh
@@ -4,7 +4,7 @@ export MATSCREEN=/home/emmanuel/Documents/material-screening
 #echo "Running on nodes:"
 #echo "${NODES}"
 
-python3 $MATSCREEN/screen.py -ppn 20 -n 20 -s $MATSCREEN/data/structures_sample.csv -m xenon krypton -t grid 
+python3 $MATSCREEN/screen.py -ppn 20 -n 20 -s $MATSCREEN/data/structures_sample.csv -m xenon -N 100 -t ads 
 
 rm -rf Movies/
 rm -rf VTK/
