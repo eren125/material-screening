069ae3a5a9e29e618c5525ec1c4dc24df8d118eb
eren125 <emmanuelren1995@gmail.com>
2023-01-20 19:04:14 +0100

    update

--- job_widom.sh
+++ job_widom.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-source ./set_environment
 #export NODES=$(srun hostname | sort | uniq)
 #echo "Running on nodes:"
 #echo "${NODES}"
 
-$MATSCREEN_PYTHON $MATSCREEN/screen.py -ppn 1 -n 1 -s $MATSCREEN/data/structures_sym.csv -m xenon -g yes -N 100000 -t widom_nogrid
+$MATSCREEN_PYTHON $MATSCREEN/screen.py -ppn 1 -n 1 -s $MATSCREEN/data/structures_sym.csv -v 20 -m xenon -g yes -N 100000 -t widom_nogrid
+if [ -f "set_environment" ]; then
+	rm set_environment
+fi
+bash $MATSCREEN/copy_env.sh set_environment
 
-rm -rf Movies/
-rm -rf VTK/
-rm -rf Restart/
-rmdir Scripts
