1d8715878ad92e14c0ad628f9a4c3e3517501375
eren125 <emmanuelren1995@gmail.com>
2023-01-23 19:01:25 +0100

    barrier_calc + radTable zeo++

--- Cpp_screening_templates/run_cgrid.sh
+++ Cpp_screening_templates/run_cgrid.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 
 STRUC_DIR=$RASPA_DIR/share/raspa/structures/cif
 
-ads_grid.out $STRUC_DIR/$1.cif $RASPA_DIR/share/raspa/forcefield/FORCE_FIELD/force_field_mixing_rules.def TEMPERATURE CUTOFF ATOMS TIMESTEP >> PATH/cpp_output_N_cycles.csv
+ads_grid.out $STRUC_DIR/$1.cif $RASPA_DIR/share/raspa/forcefield/FORCE_FIELD/force_field_mixing_rules.def $3 CUTOFF ATOMS TIMESTEP 20 0.8 >> PATH/cpp_output_N_cycles.csv
---
+++ Zeo++_screening_templates/README.md
@@ -0,0 +1,3 @@
+After generating the glost.list file. You can change the run file to add an option to read rad_uff_298K.rad, which is more adapted. See example in `run_all`
+
+For more details on how the radius tables are calculated, please look at https://github.com/eren125/zeopp_radtable
