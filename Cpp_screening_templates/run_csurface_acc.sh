e60699894c7bb10ef988598d0ef3599d80d46753
eren125 <emmanuelren1995@gmail.com>
2023-01-23 14:57:23 +0100

    compatibility

--- Cpp_screening_templates/run_csurface_acc.sh
+++ Cpp_screening_templates/run_csurface_acc.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")"
 
 STRUC_DIR=$RASPA_DIR/share/raspa/structures/cif
 
-${CPP_DIR}/bin/surface_acc.out $STRUC_DIR/$1.cif $RASPA_DIR/share/raspa/forcefield/FORCE_FIELD/force_field_mixing_rules.def TEMPERATURE CUTOFF N_cycles ATOMS TIMESTEP >> PATH/cpp_output_TIMESTEP.csv
+${CPP_DIR}/bin/surface_acc.out $STRUC_DIR/$1.cif $RASPA_DIR/share/raspa/forcefield/FORCE_FIELD/force_field_mixing_rules.def $3 CUTOFF N_cycles ATOMS TIMESTEP >> PATH/cpp_output_TIMESTEP.csv
--- Cpp_screening_templates/run_csurface_spiral.sh
+++ Cpp_screening_templates/run_csurface_spiral.sh
@@ -6,4 +6,4 @@ cd "$(dirname "$0")"
 
 STRUC_DIR=$RASPA_DIR/share/raspa/structures/cif
 
-${CPP_DIR}/bin/surface_spiral.out $STRUC_DIR/$1.cif $RASPA_DIR/share/raspa/forcefield/FORCE_FIELD/force_field_mixing_rules.def TEMPERATURE CUTOFF N_cycles ATOMS >> PATH/cpp_output_N_cycles.csv
+${CPP_DIR}/bin/surface_spiral.out $STRUC_DIR/$1.cif $RASPA_DIR/share/raspa/forcefield/FORCE_FIELD/force_field_mixing_rules.def $3 CUTOFF N_cycles ATOMS >> PATH/cpp_output_N_cycles.csv
